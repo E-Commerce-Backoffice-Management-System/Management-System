c1e324af421b9c17408a0c6c2dbfc705e1a7dac2
godchm <choehyeongmin7@gmail.com>
2026-02-19 20:06:11 +0900

    feat: Initial commit

---
+++ src/main/java/com/mangementsystem/MangementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mangementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mangementsystem/MangementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
