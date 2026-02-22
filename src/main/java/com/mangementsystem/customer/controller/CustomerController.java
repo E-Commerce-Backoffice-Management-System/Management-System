89378b1cb6ed0398cee5229598cdba36beced0f1
t2025-m0040 <e0321e@nate.com>
2026-02-22 17:26:09 +0900

    feat: customer CRUD

--- src/main/java/com/mangementsystem/customer/controller/CustomerController.java
+++ src/main/java/com/mangementsystem/customer/controller/CustomerController.java
@@ -1,8 +1,7 @@
 package com.mangementsystem.customer.controller;
 
-import com.mangementsystem.customer.dto.CreateCustomerRequest;
-import com.mangementsystem.customer.dto.CreateCustomerResponse;
-import com.mangementsystem.customer.dto.GetCustomerResponse;
+import com.mangementsystem.customer.dto.*;
+import com.mangementsystem.customer.entity.CustomerStatus;
 import com.mangementsystem.customer.service.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,12 +21,39 @@ public class CustomerController {
     public ResponseEntity<CreateCustomerResponse> createCustomer(@Valid @RequestBody CreateCustomerRequest request){
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(request));
     }
-    @GetMapping("/admins/customers/customerId")
-    public ResponseEntity<Page<GetCustomerResponse>> getCustomers(
-            @RequestParam int page,
-            @RequestParam int size
+    @GetMapping("/admins/customers")
+    public ResponseEntity<Page<GetCustomerResponse>> getAll(
+            @RequestParam(defaultValue = "") String keyword,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "createdAt") String sortBy,
+            @RequestParam(defaultValue = "desc") String direction
+            ){
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.findAll(keyword, page, size, sortBy, direction));
+    }
+    @GetMapping("/admins/customers/{customerId}")
+    public ResponseEntity<GetCustomerResponse> getOne(@PathVariable Long customerId){
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.findOne(customerId));
+    }
+    @PatchMapping("/admins/customers/{customerId}")
+    public ResponseEntity<UpdateCustomerResponse> updateCustomer(
+            @PathVariable Long customerId,
+            @RequestBody UpdateCustomerRequest request
     ){
-        return ResponseEntity.status(HttpStatus.OK).body(customerService.findAll(page, size));
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.update(customerId, request));
+    }
+    @PatchMapping("/admins/customers/{customerId}/status")
+    public ResponseEntity<Void> updateStatus(
+            @PathVariable Long customerId,
+            @RequestBody UpdateStatusRequest request
+            ){
+        customerService.updateStatus(customerId, request.getStatus());
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @DeleteMapping("/admins/customers/{customerId}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long customerId){
+        customerService.delete(customerId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
