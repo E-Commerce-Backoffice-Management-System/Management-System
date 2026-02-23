269c77363fc307df967509b8a692d93426078b1f
Poten97 <ghwjd6767@gmail.com>
2026-02-23 11:35:42 +0900

    refactor/OrderRepository

--- src/main/java/com/managementSystem/order/repository/OrderRepository.java
+++ src/main/java/com/managementSystem/order/repository/OrderRepository.java
@@ -1,16 +1,14 @@
-package com.mangementsystem.order.repository;
+package com.managementSystem.order.repository;
 
-import com.mangementsystem.order.dto.OrderListResponse;
-import com.mangementsystem.order.entity.Order;
-import com.mangementsystem.order.enums.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Query("""
+public class OrderRepository {
+    public interface OrderRepository extends JpaRepository<Order, Long> {
+        @Query("""
         SELECT new com.mangementsystem.order.dto.OrderListResponse(
             o.id,
             o.orderNumber,
@@ -36,10 +34,11 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             OR c.name LIKE %:keyword%
         )
     """)
+        Page<OrderListResponse> searchOrders(
+                @Param("keyword") String keyword,
+                @Param("status") OrderStatus status,
+                Pageable pageable
+        );
+    }
 
-    Page<OrderListResponse> searchOrders(
-            @Param("keyword") String keyword,
-            @Param("status") OrderStatus status,
-            Pageable pageable
-    );
 }
