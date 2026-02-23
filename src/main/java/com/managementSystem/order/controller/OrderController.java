168e342d41107e635d5939ef4520a79ba296195b
Poten97 <ghwjd6767@gmail.com>
2026-02-23 21:55:55 +0900

    feat/OrderController

--- src/main/java/com/managementSystem/order/controller/OrderController.java
+++ src/main/java/com/managementSystem/order/controller/OrderController.java
@@ -1,4 +1,43 @@
 package com.managementSystem.order.controller;
 
+import com.managementSystem.admin.entity.Admin;
+import com.managementSystem.order.dto.CreateOrderRequest;
+import com.managementSystem.order.dto.CreateOrderResponse;
+import com.managementSystem.order.service.OrderService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    //주문 생성 (CS 대리 주문)
+    @PostMapping("/admin/orders")
+    public ResponseEntity<CreateOrderResponse> createAdminOrder(
+            @RequestBody CreateOrderRequest request,
+            HttpSession session
+    ){
+        Admin loginAdmin = (Admin) session.getAttribute("loginAdmin");
+        // 관리자가 로그인이 상태가 아니면 에러 응답
+        if(loginAdmin == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(request, loginAdmin));
+    }
+    //주문 생성 (고객 직접 주문)
+    @PostMapping("/customer/orders")
+    public ResponseEntity<CreateOrderResponse> createCustomerOrder(
+            @RequestBody CreateOrderRequest request
+    ){
+        //고객이 직접 주문하는 거니까 관리자 정보는 null로 넘겨줌
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(request, null));
+    }
+
 }
