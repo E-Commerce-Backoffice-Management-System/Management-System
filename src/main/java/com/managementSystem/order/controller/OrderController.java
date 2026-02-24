3b91f7bcf0fd25307082d1f442d9b50dbb2117e1
Poten97 <ghwjd6767@gmail.com>
2026-02-24 15:48:37 +0900

    feat/CancelOrderService

--- src/main/java/com/managementSystem/order/controller/OrderController.java
+++ src/main/java/com/managementSystem/order/controller/OrderController.java
@@ -60,12 +60,12 @@ public class OrderController {
 
 
     // 주문 취소
-    @PatchMapping("/orders/{id}/camcel")
-    public ResponseEntity<ApiResponse<CancelOrderResponse>> cancelOrder(
-            @PathVariable Long id,
-            @Valid @RequestBody CancelOrderRequest request
-    ) {
-        return ResponseEntity.status(HttpStatus.OK).body(
-                ApiResponse.success(orderService.cancelOrder(id,request)));
-    }
+//    @PatchMapping("/orders/{id}/camcel")
+//    public ResponseEntity<ApiResponse<CancelOrderResponse>> cancelOrder(
+//            @PathVariable Long id,
+//            @Valid @RequestBody CancelOrderRequest request
+//    ) {
+//        return ResponseEntity.status(HttpStatus.OK).body(
+//                ApiResponse.success(orderService.cancelOrder(id,request)));
+//    }
 }
