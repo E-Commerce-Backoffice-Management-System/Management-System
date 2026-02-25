043f1723a92fbe58df35fe026d81a33a5dda1008
Ho-jin98 <hohoj98@naver.com>
2026-02-25 17:49:16 +0900

    feat:test

--- src/main/java/com/managementSystem/order/controller/OrderController.java
+++ src/main/java/com/managementSystem/order/controller/OrderController.java
@@ -108,13 +108,4 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
-//    // 주문 취소
-//    @PatchMapping("/orders/{id}/camcel")
-//    public ResponseEntity<ApiResponse<CancelOrderResponse>> cancelOrder(
-//            @PathVariable Long id,
-//            @Valid @RequestBody CancelOrderRequest request
-//    ) {
-//        return ResponseEntity.status(HttpStatus.OK).body(
-//                ApiResponse.success(orderService.cancelOrder(id,request)));
-//    }
 }
